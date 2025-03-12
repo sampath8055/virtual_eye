302e5ab9ed526ae0701e489f5ed8a7ce64e4c210
K H Akhil <khakhil.khakhil@gmail.com>
2025-03-12 12:58:57 +0530

    bash file updated

--- start_main.sh
+++ start_main.sh
@@ -3,14 +3,17 @@
 VENV_DIR="env"
 
 if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating Virtual Enviroment"
     python3 -m venv $VENV_DIR
 fi
 
 os_name=$(uname)
 
 if [ "$os_name" == "Linux" ] || [ "$os_name" == "Darwin" ]; then
+    echo "Activating Virtual Enviroment"
     source $VENV_DIR/bin/activate
 else
+    echo "Activating Virtual Enviroment"
     $VENV_DIR/Scripts/activate.bat
 fi
 
